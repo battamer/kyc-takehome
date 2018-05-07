5100d773af144dc57a7e5d1e7a2dbe0c8ae5e101
MB <battamer@gmail.com>
2018-05-08 02:34:02 +0300

    Moves files to make first working model

---
+++ Checker/index.js
@@ -0,0 +1,18 @@
+/*
+ * https://cloud.google.com/functions/docs/calling/storage
+*/
+
+exports.recieveNewImageMetadata = (event, callback) => {
+  const file = event.data;
+  const context = event.context;
+
+  console.log(`Event ${context.eventId}`);
+  console.log(`  Event Type: ${context.eventType}`);
+  console.log(`  Bucket: ${file.bucket}`);
+  console.log(`  File: ${file.name}`);
+  console.log(`  Metageneration: ${file.metageneration}`);
+  console.log(`  Created: ${file.timeCreated}`);
+  console.log(`  Updated: ${file.updated}`);
+
+  callback();
+};
