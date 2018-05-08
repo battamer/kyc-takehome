2a469721581ab77e12e7be1ce95409ebe3eb94c5
MB <battamer@gmail.com>
2018-05-08 03:30:55 +0300

    adds bucket

--- Checker/index.js
+++ Checker/index.js
@@ -1,8 +1,23 @@
 /*
  * https://cloud.google.com/functions/docs/calling/storage
+ * https://cloud.google.com/functions/docs/tutorials/ocr#windows
 */
+'use strict';
 
-exports.recieveNewImageMetadata = (event, callback) => {
+// [START functions_ocr_setup]
+// const config = require('./config.json');
+
+// Get a reference to the Pub/Sub component
+const pubsub = require('@google-cloud/pubsub')();
+// Get a reference to the Cloud Storage component
+const storage = require('@google-cloud/storage')();
+// Get a reference to the Cloud Vision API component
+const vision = require('@google-cloud/vision')();
+
+const Buffer = require('safe-buffer').Buffer;
+
+
+exports.recieveNewImage = (event, callback) => {
   const file = event.data;
   const context = event.context;
 
