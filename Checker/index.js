bf6b337928795b58130d3493abb3cfff19def679
MB <battamer@gmail.com>
2018-05-08 20:54:24 +0300

    adds detectTags() and implements basic functionality

--- Checker/index.js
+++ Checker/index.js
@@ -1,8 +1,9 @@
 /*
  * https://cloud.google.com/functions/docs/calling/storage
  * https://cloud.google.com/functions/docs/tutorials/ocr#windows
+ * https://github.com/thesandlord/samples/blob/master/cloud-vision-nodejs/index.js#L52
+ * https://github.com/googleapis/nodejs-vision/blob/master/samples/detect.js
 */
-'use strict';
 
 // [START functions_ocr_setup]
 // const config = require('./config.json');
@@ -14,20 +15,33 @@ const storage = require('@google-cloud/storage')();
 // Get a reference to the Cloud Vision API component
 const vision = require('@google-cloud/vision')();
 
+const acceptableTags = {'fish':1, 'bread':1, 'milk':1}
+
 const Buffer = require('safe-buffer').Buffer;
 
 
-exports.recieveNewImage = (event, callback) => {
+exports.recieveNewImageMetadata = (event, callback) => {
   const file = event.data;
   const context = event.context;
 
-  console.log(`Event ${context.eventId}`);
-  console.log(`  Event Type: ${context.eventType}`);
-  console.log(`  Bucket: ${file.bucket}`);
-  console.log(`  File: ${file.name}`);
-  console.log(`  Metageneration: ${file.metageneration}`);
-  console.log(`  Created: ${file.timeCreated}`);
-  console.log(`  Updated: ${file.updated}`);
-
+  var tags = detectTags(file.bucket, file.name)
+  console.log(`	 Tags: ${tags}`);
   callback();
 };
+
+function detectTags (bucketName, filename) {
+	var types = ['labels']
+
+	vision
+		.labelDetection({ source: { imageUri: `gs://${bucketName}/${filename}` } })
+		.then(results => { 
+			const labels = results[0].labelAnnotations;
+			console.log('labels:');
+			labels.forEach(label => {
+				if(label.description in acceptableTags && label.score >= .5)  {
+					console.log(`${label.description} ${label.score}`);
+				}
+			})
+		});
+	return ['a','b','c']
+}
